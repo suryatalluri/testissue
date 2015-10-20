6bb3d29f739866255572fd0dfdde3365bfe4f764
Surya Santosh Mani T null <surya.talluri@wavemaker.com>
2015-10-20 08:46:02 +0000

    New files added - [] Files modified - [Main.html,Main.js,page.min.html,wmProperties.js] Refactored files - [] Fixed bugs - [] Removed files - []

--- src/main/webapp/wmProperties.js
+++ src/main/webapp/wmProperties.js
@@ -1,1 +1,1 @@
-var _WM_APP_PROPERTIES = {"defaultLanguage":"en","type":"APPLICATION","homePage":"Main","platformType":"MOBILE"}
+var _WM_APP_PROPERTIES = {"defaultLanguage":"en","type":"APPLICATION","homePage":"Main","platformType":"MOBILE","activeTheme":"mobile"}
